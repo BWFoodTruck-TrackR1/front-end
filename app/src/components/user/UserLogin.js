84b171aa1bff63618fd794a49922ece329c60844
Tom Bauer <tbauer1898@gmail.com>
2020-08-24 16:48:58 -0500

    User login form outline done..

--- app/src/components/user/UserLogin.js
+++ app/src/components/user/UserLogin.js
@@ -4,68 +4,102 @@ import {useSelector} from "react-redux"
 import {useHistory, Link} from "react-router-dom"
 
 import {valueChanger, valueClearing} from "../../state-manegment/foodTruckReducer"
+import { Form } from "semantic-ui-react"
 
+
+// const UserLogin = () => {
+//     const login = useSelector(state => state.login)
+//     console.log(login)
+
+//     const {push} = useHistory()
+
+//     const valueUpdate = (event) => {
+//         const {name, value} = event.target
+//         store.dispatch(valueChanger(value,name))
+//     }
+
+//     const submit = (event) => {
+//         event.preventDefault()
+//         console.log("submitting!!!!!")
+//         store.dispatch(valueClearing())
+//         push("/Hello")
+//     }
+
+//     return (
+//         <div>
+//             <form onSubmit={submit}>
+//                 <label htmlFor="username">
+//                 Username
+//                 <div>
+//                 <input
+//                 type="text"
+//                 name="username"
+//                 id="username"
+//                 placeholder="Enter Your Username"
+//                 value={login.username}
+//                 onChange={valueUpdate}
+//                  />
+//                 </div>
+                    
+//                 </label>
+
+//                 <label htmlFor="password">
+//                 Password
+//                 <div>
+//                 <input
+//                 type="text"
+//                 name="password"
+//                 id="password"
+//                 placeholder="Enter Your Password"
+//                 value={login.password}
+//                 onChange={valueUpdate}
+//                  />
+//                 </div>
+                    
+//                 </label>
+//                 <button type="submit">Login!</button>
+//             </form>
+//             dont have an accout? <Link to="/UserRegister" >
+//                 register
+//             </Link>
+            
+//         </div>
+//     )
+// }
+
+// export default UserLogin
 
 const UserLogin = () => {
-    const login = useSelector(state => state.login)
-    console.log(login)
 
-    const {push} = useHistory()
-
-    const valueUpdate = (event) => {
-        const {name, value} = event.target
-        store.dispatch(valueChanger(value,name))
-    }
-
-    const submit = (event) => {
-        event.preventDefault()
-        console.log("submitting!!!!!")
-        store.dispatch(valueClearing())
-        push("/Hello")
-    }
-
-    return (
-        <div>
-            <form onSubmit={submit}>
-                <label htmlFor="username">
-                Username
-                <div>
-                <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Enter Your Username"
-                value={login.username}
-                onChange={valueUpdate}
-                 />
-                </div>
+    return(
+        <div className='userLogin__container'>
+            <form>
+                <label>Username:&nbsp;
+                    <input
+                    name='username'
+                    type='text'
+                    placeholder='Username'
+                    id='username'
                     
+                    />
                 </label>
 
-                <label htmlFor="password">
-                Password
-                <div>
-                <input
-                type="text"
-                name="password"
-                id="password"
-                placeholder="Enter Your Password"
-                value={login.password}
-                onChange={valueUpdate}
-                 />
-                </div>
-                    
+                <label>Password:&nbsp;
+                    <input 
+                    name='password'
+                    type='password'
+                    placeholder='Enter Password'
+                    id='password'
+                    />
+
                 </label>
-                <button type="submit">Login!</button>
+                <button >Login</button>
             </form>
-            dont have an accout? <Link to="/UserRegister" >
-                register
-            </Link>
-            
+            <p>Don't have an account? <Link to='/UserRegister'>Register Here</Link></p>
         </div>
     )
 }
 
-export default UserLogin
+export default UserLogin;
 
 
